5178d39b7bf60f2b789a61b6cfb7f928641eeca0
Matane <permatamailbox@gmail.com>
2025-12-31 15:21:38 +0700

    Add files via upload

--- Kazefuri/build.gradle.kts
+++ Kazefuri/build.gradle.kts
@@ -1,26 +1,36 @@
-// use an integer for version numbers
-version = 2
+plugins {
+    id 'com.android.library'
+    id 'org.jetbrains.kotlin.android'
+}
 
-cloudstream {
-    language = "id"
-    // All of these properties are optional, you can safely remove them
+android {
+    compileSdk 34
+    
+    defaultConfig {
+        minSdk 21
+        targetSdk 34
+    }
+    
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+}
 
-    // description = "Lorem Ipsum"
-     authors = listOf("Hexated", "TeKuma25")
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-     * */
-    status = 1 // will be 3 if unspecified
-    tvTypes = listOf(
-        "AnimeMovie",
-        "OVA",
-        "Anime",
-    )
+dependencies {
+    implementation 'com.lagradost:cloudstream3:4.6.0'
+}
 
     iconUrl = "https://www.google.com/s2/favicons?domain=sv3.kazefuri.cloud&sz=%size%"
 }
