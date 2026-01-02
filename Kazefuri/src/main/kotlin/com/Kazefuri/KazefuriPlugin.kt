9a8486a11d4433d5de93506380791de56838d029
Matane <permatamailbox@gmail.com>
2026-01-02 13:18:38 +0700

    Add files via upload

--- Kazefuri/src/main/kotlin/com/Kazefuri/KazefuriPlugin.kt
+++ Kazefuri/src/main/kotlin/com/Kazefuri/KazefuriPlugin.kt
@@ -1,14 +1,12 @@
-package com.kazefuri
+package com.Kazefuri
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class KazefuriPlugin: Plugin() {
-    override fun load(context: Context) {
-        // Pastikan nama class "Kazefuri()" sesuai dengan yang ada di Kazefuri.kt
+    // Gunakan Any? agar tidak perlu import android.content.Context
+    override fun load(context: Any?) {
         registerMainAPI(Kazefuri())
     }
 }
-
-
