2c833387243ac50207af064a0abe543bc759fbff
Matane <permatamailbox@gmail.com>
2026-01-02 15:23:54 +0700

    Create AnichinPlugin.kt

---
+++ Anichin/src/main/kotlin/com/Anichin/AnichinPlugin.kt
@@ -0,0 +1,13 @@
+package com.Anichin
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class KazefuriPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Baris ini memanggil class Kazefuri() dari file sebelah
+        registerMainAPI(Anichin())
+    }
+}
