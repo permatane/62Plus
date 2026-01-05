d87fadd897b7ab20b98af6ada6ff45078ae7ce06
Matane <permatamailbox@gmail.com>
2026-01-05 17:09:11 +0700

    Update Auratail.kt

--- Anichin/src/main/kotlin/com/Anichin/Auratail.kt
+++ Anichin/src/main/kotlin/com/Anichin/Auratail.kt
@@ -7,14 +7,7 @@ class Auratail : Anichin() {
     override var mainUrl              = "https://auratail.vip/"
     override var name                 = "Auratail"
     
-
     override val mainPage = mainPageOf(
         "anime/?status=&type=&order=update" to "Update Terbaru",
     )
-    
-        override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        val document = app.get("$mainUrl/${request.data}&page=$page").documentLarge
-        val home     = document.select("div.listupd > article").mapNotNull { it.toSearchResult() }
-        return newHomePageResponse(request.name, home)
-    }
 }
