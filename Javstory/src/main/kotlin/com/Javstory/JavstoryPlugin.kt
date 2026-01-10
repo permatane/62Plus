13080c33befaae956e1c05e9a440ad373e40d183
Matane <permatamailbox@gmail.com>
2026-01-10 13:31:52 +0700

    Update JavstoryPlugin.kt

--- Javstory/src/main/kotlin/com/Javstory/JavstoryPlugin.kt
+++ Javstory/src/main/kotlin/com/Javstory/JavstoryPlugin.kt
@@ -4,12 +4,15 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.lagradost.cloudstream3.extractors.StreamTape
+import com.lagradost.cloudstream3.extractors.SoraPlay
+import com.lagradost.cloudstream3.extractors.Voe
 
 @CloudstreamPlugin
 class JavstoryPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(Javstory())
         registerExtractorAPI(StreamTape())
-    
+        registerExtractorAPI(SoraPlay())
+        registerExtractorAPI(Voe())
     }
 }
